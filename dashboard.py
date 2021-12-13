a55b9cbb8410104dcb896c31e571f84f24277da2
Jeff Tahiata <jeff.tahiata@laposte.net>
2021-12-13 16:41:21 +0100

    Modif plotly

--- dashboard.py
+++ dashboard.py
@@ -178,8 +178,7 @@ if option == 'General statistics':
         
         st.subheader('Crossed stats between features')
         # fig, ax = plt.subplots()
-        # fig = px.scatter(x = df_original.loc[:,feat1], y = df_original.loc[:,feat2])
-        fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-        # plt.xlabel(str(feat1))
-        # plt.ylabel(str(feat2))
+        fig = px.scatter(x = df_original.loc[:,feat1], y = df_original.loc[:,feat2])
+        plt.xlabel(str(feat1))
+        plt.ylabel(str(feat2))
         st.plotly_chart(fig)

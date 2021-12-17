03ba4b1dc3f1e311de206b3cee4b42735f7b7c1c
Jeff Tahiata <jeff.tahiata@laposte.net>
2021-12-17 15:09:09 +0100

    Expected_value et Shap_value importés de l'API

--- api.py
+++ api.py
@@ -5,34 +5,46 @@ Created on Sat Nov 27 16:10:13 2021
 @author: Jeff
 """
 
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
 import joblib
-import numpy as np
+import shap
 import pandas as pd
 
 app = Flask(__name__)
 
 model = joblib.load('/home/jtahiata/mysite/functions/loan_model.joblib')
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     data_json = data['data']
     data_val = [list(data_json.values())]
+    df = pd.json_normalize(data_json)
+
+    # print(data_val)
     predict_val = model.predict(data_val)
     prediction = predict_val.tolist()
+    # print(prediction)
     proba_val = model.predict_proba(data_val)
-    probabilite = proba_val.tolist()
+    probability = proba_val.tolist()
+    # print(probability)
 
-    return jsonify(Prediction=prediction, Probabilite=probabilite)
-    # print(data)
-    # return Response(status=200)
-    # prediction = np.array2string(model.predict(data))
-    # data = json.loads(elevations)
+    # Shap init JavaScript visualization code to notebook, Calculate Shap values
+    shap.initjs()
+    explainer = shap.TreeExplainer(model)
+    expected_value = explainer.expected_value[1]
+    shap_values_ = explainer.shap_values(df.values)[0][0]
+    shap_values = []
 
-    # prediction = model.predict(pd.DataFrame.from_dict(data, orient="index").values)
+    for string in shap_values_:
+        new_string = float(str(string).replace("#12", ""))
+        shap_values.append(new_string)
 
-    # return jsonify(prediction)
+    print(data_json)
+    print(df.values)
+    print(shap_values)
+
+    return jsonify(Prediction=prediction, Probability=probability, Expected_value=expected_value, Shap_values=shap_values)
 
 if __name__ == '__main__':
     app.run(debug=True)

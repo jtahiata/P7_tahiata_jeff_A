e63d4a6a0b9459ad034033f52a4a6332f2dcb0d0
Jeff Tahiata <jeff.tahiata@laposte.net>
2021-12-20 17:34:22 +0100

    Expected_value et Shap_value importés de l'API

--- dashboard.py
+++ dashboard.py
@@ -132,7 +132,7 @@ if option == 'Solvability prediction':
         
     # Calculate Shap values
     expected_value = predict['Expected_value']
-    shap_values = list(json.loads(predict['Shap_values'])["0"].values())
+    shap_values = list(json.loads(predict['Shap_values'])["0"].values()).flatten()
     
     st.write(shap_values)
     
